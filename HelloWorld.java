09c893ee4a30b70281aaacac2d67ad514c9597c2
Ichsannf <ichsanfauzi354@gmai.com>
2020-09-16 11:30:06 +0700

    tugas jurnal-2

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Nama: Ichsan!");
+		System.out.println("NIM: 6706202072");
+		System.out.println("Kelas: D3IF4402");
+	}
+}
